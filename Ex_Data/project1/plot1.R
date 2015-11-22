fdbeaae428751e6b17559d2223b323348789faf2
BigBorg <770188954@qq.com>
2015-11-22 17:08:14 +0800

    Ex_data
    Ex_data project 1 and 2

---
+++ Ex_Data/project1/plot1.R
@@ -0,0 +1,26 @@
+	#Download and unzip dataset
+	if(!file.exists("course project1")){
+		dir.create("course project1");
+	}
+	download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = "./course project1/dataset.zip");
+	setwd("./course project1");
+	unzip("dataset.zip");
+	
+	#Set language,load packages and process data
+	mylocal<-Sys.getlocale("LC_ALL");
+	Sys.setlocale("LC_ALL", "English");
+	library(dplyr);
+	library(lubridate);
+	dataframe<-read.csv("household_power_consumption.txt",header=TRUE,nrow=69516,sep=";",stringsAsFactors=FALSE);
+	tbldata<-tbl_df(dataframe);
+	feb<-filter(tbldata,Date=="1/2/2007"|Date=="2/2/2007");
+	feb<-mutate(feb,Date=as.Date(Date,"%d/%m/%Y"),Global_active_power=as.numeric(Global_active_power),Sub_metering_1=as.numeric(Sub_metering_1),Sub_metering_2=as.numeric(Sub_metering_2),Sub_metering_3=as.numeric(Sub_metering_3),Voltage=as.numeric(Voltage),Global_reactive_power=as.numeric(Global_reactive_power));
+	xval<-ymd_hms(paste(feb$Date,feb$Time,sep=" "));
+	
+	#plot1
+	png("plot1.png");
+	hist(feb$Global_active_power,main="Global Active Power",xlab="Global Active Power(kilowatts)",col="red");
+	dev.off();
+	
+	#restore language
+	Sys.setlocal("LC_ALL", mylocal);
---
+++ Ex_Data/project2/script.R
@@ -0,0 +1,60 @@
+if(!file.exists("projects")){
+	dir.create("project2");
+	setwd("./project2");
+	download.files("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",destfile="Data.zip");
+	unzip("Data.zip");
+}
+
+NEI <- readRDS("summarySCC_PM25.rds");
+SCC <- readRDS("Source_Classification_Code.rds");
+
+library(dplyr);
+library(ggplot2);
+
+#question 1/plot 1 : Yes, the Emissions are decreasing according to the plot.
+groupedNEI<-group_by(NEI,year);
+sumEmissions<-summarize(groupedNEI,sum(Emissions));
+png("plot1.png");
+plot(sumEmissions$year,sumEmissions$`sum(Emissions)`,xlab = "year",ylab="Emissions",main="Emissions Trend(USA)",type="l");
+dev.off();
+
+#question 2/plot 2: Decreasing
+baltimore<-filter(groupedNEI,fips=="24510");	##This line of code might take some time, please be patient...
+sumEmissionsbal<-summarize(baltimore,sum(Emissions));
+png("plot2.png");
+plot(sumEmissionsbal$year,sumEmissionsbal$`sum(Emissions)`,xlab = "year",ylab="Emissions",main="Emission Trend(Baltimore)",type="l");
+dev.off();
+
+#question 3/plot 3: Emission of type "POINT" is increasing, other types decreasing.
+png("plot3.png");
+sumbaltype<-summarize(group_by(baltimore,year,type),Emissions=sum(Emissions));
+xyplot(Emissions~year|type,data = sumbaltype,main="Emission Trend(type,Baltimore)",panel=function(x,y,...){panel.xyplot(x,y,...);panel.lmline(x,y);});
+dev.off();
+
+#question 4/plot 4: Decreasing
+png("plot4.png");
+coal<-grep("^Fuel (.*) Coal",SCC$EI.Sector);
+filt<-SCC[coal,1];
+coalData<-filter(NEI,SCC %in% filt);
+groupedcoalData<-group_by(coalData,year);
+sumcoal<-summarize(groupedcoalData,Emissions=sum(Emissions));
+q<-ggplot(data=sumcoal,aes(year,Emissions)) + geom_point() + geom_line() +geom_smooth(method="lm") + labs(x="year",y="Total Emissions") + labs(title="Emission Trend(coal,US)");
+print(q);
+dev.off();
+
+#question 5/plot 5: Decreasing
+png("plot5.png");
+motorbal<-filter(baltimore,type=="ON-ROAD");
+groupedmotorbal<-group_by(motorbal,year);
+summotorbal<-summarize(groupedmotorbal,Emissions=sum(Emissions));
+q<-ggplot(data = summotorbal,aes(year,Emissions)) + geom_point() + geom_line() + geom_smooth(method="lm") + labs(title="Emission Trend(motor vehicle,Baltimore)");
+print(q);
+dev.off();
+
+#question 6/plot 6: 
+png("plot6.png");
+ballos<-filter(NEI,fips=="24510"|fips=="06037");
+sumballos<-summarize(group_by(ballos,fips,year),Emissions=sum(Emissions));
+q<-ggplot(data=sumballos,aes(year,Emissions)) + geom_point(aes(color=factor(fips,labels=c("Los Angeles","Baltimore")))) + geom_line( aes(color=factor(fips,labels = c("Los Angeles","Baltimore")))) + labs(color="City") +labs(title="Emission Trend(motor vehicle,Los Angeles & Baltimore)");
+print(q);
+dev.off();
